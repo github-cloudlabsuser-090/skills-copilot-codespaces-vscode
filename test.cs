e7d79b7ce4498037bac0b8e2f85cb1fb22793fbb
github-cloudlabsuser-090 <80266034+github-cloudlabsuser-090@users.noreply.github.com>
2023-12-13 16:30:33 +0000

    Copilot fourth commit

---
+++ test.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Linq;
+
+class Program
+{
+    const int MAX = 100;
+
+    static void Main()
+    {
+        Console.Write("Enter the number of elements (1-100): ");
+        if (!int.TryParse(Console.ReadLine(), out int n) || n < 1 || n > MAX)
+        {
+            Console.WriteLine("Invalid input. Please provide a digit ranging from 1 to 100.");
+            Environment.Exit(1);
+        }
+
+        int[] arr = new int[n];
+
+        Console.WriteLine($"Enter {n} integers:");
+        for (int i = 0; i < n; i++)
+        {
+            if (!int.TryParse(Console.ReadLine(), out arr[i]))
+            {
+                Console.WriteLine("Invalid input. Please enter valid integers.");
+                Environment.Exit(1);
+            }
+        }
+
+        int total = arr.Sum();
+
+        Console.WriteLine($"Sum of the numbers: {total}");
+    }
+}
